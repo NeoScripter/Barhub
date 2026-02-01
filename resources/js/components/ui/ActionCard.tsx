6c39d34c68f4d23530eac8f45e0c4e7e6f8d4e15
Neoscripter <sange0337@gmail.com>
2026-02-01 20:52:11 +0800

    feat:make action card draft component

---
+++ resources/js/components/ui/ActionCard.tsx
@@ -0,0 +1,60 @@
+import { cn } from '@/lib/utils';
+import { NodeProps } from '@/types/ui';
+import { LucideIcon, Plus } from 'lucide-react';
+import { FC } from 'react';
+
+const ActionCard: FC<NodeProps> = ({ className, children }) => {
+    return (
+        <article
+            className={cn(
+                'flex w-max flex-col items-center justify-center gap-3 rounded-xl border border-gray-200/40 bg-white px-3 pt-5 pb-6 text-primary shadow-xl sm:gap-3.5 sm:px-3.5 sm:pt-5.5 sm:pb-7 xl:gap-4.5 xl:px-4 xl:pt-12 xl:pb-8.5',
+                className,
+            )}
+        >
+            {children}
+        </article>
+    );
+};
+
+export default ActionCard;
+
+type ActionCardIconProps = { icon: LucideIcon; className?: string };
+
+export function ActionCardIcon({ icon, className }: ActionCardIconProps) {
+    const Icon = icon;
+    return <Icon className={cn('size-9 sm:size-10 xl:size-12', className)} />;
+}
+
+type ActionCardBtnProps = NodeProps<{ onClick: () => void }>;
+
+export function ActionCardBtn({
+    children,
+    className,
+    onClick,
+}: ActionCardBtnProps) {
+    return (
+        <button
+            onClick={onClick}
+            className={cn(
+                'inline-flex items-center justify-center gap-1.5 rounded-full bg-primary py-2 pr-6 pl-4 text-xs text-white hover:opacity-75 sm:text-sm xl:py-2.5 [&>svg]:size-3.5 sm:[&>svg]:size-4.5',
+                className,
+            )}
+        >
+            <Plus />
+            {children}
+        </button>
+    );
+}
+
+export function ActionCardTitle({ children, className }: NodeProps) {
+    return (
+        <p
+            className={cn(
+                '-mt-2 text-center font-bold text-foreground uppercase sm:text-xl xl:-mt-3',
+                className,
+            )}
+        >
+            {children}
+        </p>
+    );
+}
--- resources/js/pages/HomePage.tsx
+++ resources/js/pages/HomePage.tsx
@@ -1,6 +1,12 @@
+import ActionCard, {
+    ActionCardBtn,
+    ActionCardIcon,
+    ActionCardTitle,
+} from '@/components/ui/ActionCard';
 import Badge from '@/components/ui/Badge';
 import { Button } from '@/components/ui/Button';
 import AppLayout from '@/layouts/app/AdminLayout';
+import { Briefcase } from 'lucide-react';
 
 const HomePage = () => {
     return (
@@ -13,9 +19,14 @@ const HomePage = () => {
                 {/* <Trash2 /> */}
                 This is a button
             </Button>
-            <Badge variant='danger'>
-                Off
-            </Badge>
+            <Badge variant="danger">Off</Badge>
+            <ActionCard>
+                <ActionCardIcon icon={Briefcase} />
+                <ActionCardTitle>Спикеры</ActionCardTitle>
+                <ActionCardBtn onClick={() => {}}>
+                    Добавить событие
+                </ActionCardBtn>
+            </ActionCard>
         </AppLayout>
     );
 };
