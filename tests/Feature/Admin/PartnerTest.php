2f0387827e96488d0f26ecd317c2cb9f98cd8613
Neoscripter <sange0337@gmail.com>
2026-03-10 15:58:45 +0800

    feat:write partner test suite description

---
+++ tests/Feature/Admin/PartnerTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+
+describe('Admin Partner Test', function (): void {
+
+    it('renders the partners index page', function () {});
+    it('renders the partners edit page', function () {});
+    it('sorts tasks by company name in asc and desc order', function () {});
+    it('sorts tasks by title in asc and desc order', function () {});
+    it('sorts tasks by deadline in asc and desc order', function () {});
+    it('sorts tasks by status in asc and desc order', function () {});
+    it('displays the comments on the edit page from oldest to newest', function () {});
+    it('successfully updates the status of a task', function () {});
+    it('does not display complete tasks on the index page', function () {});
+    it('displays the correct number of tasks in each category', function () {});
+    it('displays only the tasks that belong to this exhibition', function () {});
+    it('allows super admin to enter this page', function () {});
+    it('allows super admin to update the status of a task', function () {});
+    it('does not allow admins without access to this exhibition to enter this page', function () {});
+    it('does not allow admins without access to update the status of a task', function () {});
+    it('does not allow exponents to enter this page', function () {});
+    it('does not allow exponents to update the status of a task', function () {});
+    it('does not allow users to enter this page', function () {});
+    it('does not allow users to update the status of a task', function () {});
+    it('does not allow unregistered users to enter this page', function () {});
+    it('does not allow unregistered users to update the status of a task', function () {});
+});
