2795779f2717275dea11c950cc47312216884ebd
Neoscripter <sange0337@gmail.com>
2026-03-12 22:39:11 +0800

    style:add icons to the links in the public links page

--- resources/js/pages/admin/Links/Index.tsx
+++ resources/js/pages/admin/Links/Index.tsx
@@ -1,14 +1,16 @@
 import GridLayout from '@/components/layout/GridLayout';
 import ActionCard from '@/components/ui/ActionCard';
+import CompanyController from '@/wayfinder/App/Http/Controllers/Admin/CompanyController';
 import EventController from '@/wayfinder/App/Http/Controllers/Admin/EventController';
 import PersonController from '@/wayfinder/App/Http/Controllers/Admin/PersonController';
-import CompanyController from '@/wayfinder/App/Http/Controllers/Admin/CompanyController';
 import { App } from '@/wayfinder/types';
+import { BriefcaseBusiness, Calendar, LucideIcon, User } from 'lucide-react';
 import { FC } from 'react';
 import LinkSelector, { LinkOptionType } from './partials/LinkSelector';
 
 type CardLinkType = {
     id: string;
+    icon: LucideIcon;
     options: LinkOptionType[];
     label: string;
     name: string;
@@ -23,6 +25,7 @@ const Index: FC<App.Http.Controllers.Admin.LinkController> = ({
     const cards: CardLinkType[] = [
         {
             id: crypto.randomUUID(),
+            icon: Calendar,
             options: events,
             label: 'События',
             name: 'событие',
@@ -30,6 +33,7 @@ const Index: FC<App.Http.Controllers.Admin.LinkController> = ({
         },
         {
             id: crypto.randomUUID(),
+            icon: User,
             options: people,
             label: 'Спикеры',
             name: 'спикера',
@@ -37,6 +41,7 @@ const Index: FC<App.Http.Controllers.Admin.LinkController> = ({
         },
         {
             id: crypto.randomUUID(),
+            icon: BriefcaseBusiness,
             options: companies,
             label: 'Компании',
             name: 'компанию',
@@ -49,6 +54,7 @@ const Index: FC<App.Http.Controllers.Admin.LinkController> = ({
             {cards.map((card) => (
                 <li key={card.id}>
                     <ActionCard>
+                        <ActionCard.Icon icon={card.icon}/ >
                         <ActionCard.Title>{card.label}</ActionCard.Title>
                         <LinkSelector
                             options={card.options}
