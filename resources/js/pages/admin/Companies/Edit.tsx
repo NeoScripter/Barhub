a86bad37baa660dadcd9b02f71cdf8fb1c5506d6
Neoscripter <sange0337@gmail.com>
2026-03-16 16:02:21 +0800

    feat:write tests for the admin admin page

--- resources/js/pages/admin/Companies/Edit.tsx
+++ resources/js/pages/admin/Companies/Edit.tsx
@@ -20,32 +20,23 @@ import AccentHeading from '@/components/ui/AccentHeading';
 
 const Edit: FC<Inertia.Pages.Admin.Companies.Edit> = ({
     company,
-    exhibition,
     tags,
 }) => {
-    const { data, setData, post, processing, errors, progress } = useForm({
+    const { data, setData, post, processing, errors } = useForm({
         _method: 'PUT',
         public_name: company.public_name,
         legal_name: company.legal_name,
-        description: company.description,
-        phone: company.phone,
-        email: company.email,
-        site_url: company.site_url ?? '',
-        instagram: company.instagram ?? '',
-        telegram: company.telegram ?? '',
         stand_code: String(company.stand_code),
         show_on_site: !!company.show_on_site,
-        activities: company.activities ?? '',
         tags:
             company.tags?.map((t: { id: number }) => t.id) ?? ([] as number[]),
-        logo: null as File | null,
     });
 
     const [isDeleting, setIsDeleting] = useState(false);
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
-        post(`/admin/${exhibition.id}/companies/${company.id}`, {
+        post(`/admin/companies/${company.id}`, {
             onSuccess: () => toast.success('Компания успешно обновлена'),
         });
     };
@@ -53,7 +44,7 @@ const Edit: FC<Inertia.Pages.Admin.Companies.Edit> = ({
     const handleDelete = () => {
         setIsDeleting(true);
         router.delete(
-            `/admin/${exhibition.id}/companies/${company.id}`,
+            `/admin/companies/${company.id}`,
             {
                 onSuccess: () => toast.success('Компания успешно удалена'),
                 onError: () => {
@@ -69,7 +60,7 @@ const Edit: FC<Inertia.Pages.Admin.Companies.Edit> = ({
             <div className="mx-auto w-full max-w-250">
                 <div className="mb-8 flex flex-col items-start justify-between gap-4 sm:flex-row sm:items-center">
                     <CopyLinkBtn
-                        url={`${window.location.origin}/${exhibition.id}/companies/${company.id}`}
+                        url={`${window.location.origin}/companies/${company.id}`}
                     />
 
                     <DeleteAlertDialog
@@ -140,100 +131,6 @@ const Edit: FC<Inertia.Pages.Admin.Companies.Edit> = ({
                             <InputError message={errors.legal_name} />
                         </div>
 
-                        <ImgInput
-                            key="logo-input"
-                            progress={progress}
-                            label="Логотип"
-                            isEdited={true}
-                            onChange={(file) => setData('logo', file)}
-                            src={company.logo?.webp}
-                            error={errors.logo}
-                        />
-
-                        <div className="grid gap-2 md:col-span-2">
-                            <Label htmlFor="description">Описание</Label>
-                            <Textarea
-                                id="description"
-                                required
-                                value={data.description}
-                                onChange={(e) =>
-                                    setData('description', e.target.value)
-                                }
-                                className="max-w-full"
-                            />
-                            <InputError message={errors.description} />
-                        </div>
-
-                        <div className="grid gap-2">
-                            <Label htmlFor="phone">Телефон</Label>
-                            <Input
-                                id="phone"
-                                type="text"
-                                required
-                                value={data.phone}
-                                onChange={(e) =>
-                                    setData('phone', e.target.value)
-                                }
-                            />
-                            <InputError message={errors.phone} />
-                        </div>
-
-                        <div className="grid gap-2">
-                            <Label htmlFor="email">Email</Label>
-                            <Input
-                                id="email"
-                                type="email"
-                                required
-                                value={data.email}
-                                onChange={(e) =>
-                                    setData('email', e.target.value)
-                                }
-                            />
-                            <InputError message={errors.email} />
-                        </div>
-
-                        <div className="grid gap-2">
-                            <Label htmlFor="site_url">Сайт</Label>
-                            <Input
-                                id="site_url"
-                                type="url"
-                                value={data.site_url}
-                                onChange={(e) =>
-                                    setData('site_url', e.target.value)
-                                }
-                                placeholder="https://example.com"
-                            />
-                            <InputError message={errors.site_url} />
-                        </div>
-
-                        <div className="grid gap-2">
-                            <Label htmlFor="instagram">Instagram</Label>
-                            <Input
-                                id="instagram"
-                                type="text"
-                                value={data.instagram}
-                                onChange={(e) =>
-                                    setData('instagram', e.target.value)
-                                }
-                                placeholder="@username"
-                            />
-                            <InputError message={errors.instagram} />
-                        </div>
-
-                        <div className="grid gap-2">
-                            <Label htmlFor="telegram">Telegram</Label>
-                            <Input
-                                id="telegram"
-                                type="text"
-                                value={data.telegram}
-                                onChange={(e) =>
-                                    setData('telegram', e.target.value)
-                                }
-                                placeholder="@username"
-                            />
-                            <InputError message={errors.telegram} />
-                        </div>
-
                         <div className="grid gap-2">
                             <Label htmlFor="stand_code">Номер стенда</Label>
                             <Input
@@ -263,24 +160,10 @@ const Edit: FC<Inertia.Pages.Admin.Companies.Edit> = ({
 
                         <div className="md:col-span-2">
                             <RadioCheckbox
+                                label='Показывать на сайте'
                                 value={data.show_on_site}
                                 onChange={(val) => setData('show_on_site', val)}
                             />
-                        </div>
-
-                        <div className="grid gap-2 md:col-span-2">
-                            <Label htmlFor="activities">
-                                Активности на стенде
-                            </Label>
-                            <Textarea
-                                id="activities"
-                                value={data.activities}
-                                onChange={(e) =>
-                                    setData('activities', e.target.value)
-                                }
-                                className="max-w-full"
-                            />
-                            <InputError message={errors.activities} />
                         </div>
                     </div>
 
--- tests/Feature/Admin/AdminAdminTest.php
+++ tests/Feature/Admin/AdminAdminTest.php
@@ -1,25 +1,283 @@
 <?php
-
 declare(strict_types=1);
 
-describe('Admin Company Test - Access Control', function (): void {
-    it('allows SUPER ADMIN to enter the index page');
-    it('allows SUPER ADMIN to add an admin');
-    it('allows SUPER ADMIN to delete an admin');
-    it('forbids ADMIN from entering the index page');
-    it('forbids ADMIN from adding an admin');
-    it('forbids ADMIN from deleting an admin');
-    it('forbids EXPONENT from entering the index page');
-    it('forbids EXPONENT from adding an admin');
-    it('forbids EXPONENT from deleting an admin');
-    it('forbids USER from entering the index page');
-    it('forbids USER from adding an admin');
-    it('forbids USER from deleting an admin');
-    it('forbids guest users from entering the index page');
-    it('forbids guest users from adding an admin');
-    it('forbids guest users from deleting an admin');
-    it('can assign one admin to multiple exhibitions');
-    it('does not downgrade the admin to the user after deleting them if they still have another exhibition assinged');
-    it('downgrades the admin to the user after deleting them if they do not have another exhibition assinged');
-    it('upgrades the user to the admin after assigning an exhibition to it');
+use App\Enums\UserRole;
+use App\Models\Exhibition;
+use App\Models\User;
+
+use function Pest\Laravel\get;
+
+describe('Admin Admin Page Test', function (): void {
+    beforeEach(function (): void {
+        $this->superAdmin = User::factory()->create();
+        $this->superAdmin->assignRole(UserRole::SUPER_ADMIN);
+        $this->exhibition = Exhibition::factory()->create();
+        $this->route = "/admin/exhibitions/{$this->exhibition->id}/admins";
+    });
+
+    it('allows SUPER ADMIN to enter the index page', function (): void {
+        $this->actingAs($this->superAdmin)
+            ->get($this->route)
+            ->assertOk()
+            ->assertInertia(
+                fn($page) => $page
+                    ->component('admin/Admins/Index')
+                    ->has('exhibition')
+                    ->has('admins')
+                    ->has('users')
+            );
+    });
+
+    it('allows SUPER ADMIN to add an admin', function (): void {
+        $user = User::factory()->create();
+        $user->assignRole(UserRole::USER);
+
+        $this->actingAs($this->superAdmin)
+            ->put("{$this->route}/{$user->id}")
+            ->assertRedirect();
+
+        $user->refresh();
+        $this->expect($user->role)->toBe(UserRole::ADMIN);
+        $this->assertDatabaseHas('exhibition_user', [
+            'exhibition_id' => $this->exhibition->id,
+            'user_id'       => $user->id,
+        ]);
+    });
+
+    it('allows SUPER ADMIN to delete an admin', function (): void {
+        $user = User::factory()->create();
+        $user->assignRole(UserRole::ADMIN);
+        $this->exhibition->users()->attach($user->id);
+
+        $this->actingAs($this->superAdmin)
+            ->delete("{$this->route}/{$user->id}")
+            ->assertRedirect();
+
+        $this->assertDatabaseMissing('exhibition_user', [
+            'exhibition_id' => $this->exhibition->id,
+            'user_id'       => $user->id,
+        ]);
+    });
+
+    it('forbids ADMIN from entering the index page', function (): void {
+        $admin = User::factory()->create();
+        $admin->assignRole(UserRole::ADMIN);
+
+        $this->actingAs($admin)
+            ->get($this->route)
+            ->assertForbidden();
+    });
+
+    it('forbids ADMIN from adding an admin', function (): void {
+        $admin = User::factory()->create();
+        $admin->assignRole(UserRole::ADMIN);
+        $user = User::factory()->create();
+        $user->assignRole(UserRole::USER);
+
+        $this->actingAs($admin)
+            ->put("{$this->route}/{$user->id}")
+            ->assertForbidden();
+
+        $user->refresh();
+        $this->expect($user->role)->toBe(UserRole::USER);
+    });
+
+    it('forbids ADMIN from deleting an admin', function (): void {
+        $admin = User::factory()->create();
+        $admin->assignRole(UserRole::ADMIN);
+        $target = User::factory()->create();
+        $target->assignRole(UserRole::ADMIN);
+        $this->exhibition->users()->attach($target->id);
+
+        $this->actingAs($admin)
+            ->delete("{$this->route}/{$target->id}")
+            ->assertForbidden();
+
+        $this->assertDatabaseHas('exhibition_user', [
+            'exhibition_id' => $this->exhibition->id,
+            'user_id'       => $target->id,
+        ]);
+    });
+
+    it('forbids EXPONENT from entering the index page', function (): void {
+        $exponent = User::factory()->create();
+        $exponent->assignRole(UserRole::EXPONENT);
+
+        $this->actingAs($exponent)
+            ->get($this->route)
+            ->assertForbidden();
+    });
+
+    it('forbids EXPONENT from adding an admin', function (): void {
+        $exponent = User::factory()->create();
+        $exponent->assignRole(UserRole::EXPONENT);
+        $user = User::factory()->create();
+        $user->assignRole(UserRole::USER);
+
+        $this->actingAs($exponent)
+            ->put("{$this->route}/{$user->id}")
+            ->assertForbidden();
+
+        $user->refresh();
+        $this->expect($user->role)->toBe(UserRole::USER);
+    });
+
+    it('forbids EXPONENT from deleting an admin', function (): void {
+        $exponent = User::factory()->create();
+        $exponent->assignRole(UserRole::EXPONENT);
+        $target = User::factory()->create();
+        $target->assignRole(UserRole::ADMIN);
+        $this->exhibition->users()->attach($target->id);
+
+        $this->actingAs($exponent)
+            ->delete("{$this->route}/{$target->id}")
+            ->assertForbidden();
+
+        $this->assertDatabaseHas('exhibition_user', [
+            'exhibition_id' => $this->exhibition->id,
+            'user_id'       => $target->id,
+        ]);
+    });
+
+    it('forbids USER from entering the index page', function (): void {
+        $user = User::factory()->create();
+        $user->assignRole(UserRole::USER);
+
+        $this->actingAs($user)
+            ->get($this->route)
+            ->assertForbidden();
+    });
+
+    it('forbids USER from adding an admin', function (): void {
+        $user = User::factory()->create();
+        $user->assignRole(UserRole::USER);
+        $target = User::factory()->create();
+        $target->assignRole(UserRole::USER);
+
+        $this->actingAs($user)
+            ->put("{$this->route}/{$target->id}")
+            ->assertForbidden();
+
+        $target->refresh();
+        $this->expect($target->role)->toBe(UserRole::USER);
+    });
+
+    it('forbids USER from deleting an admin', function (): void {
+        $user = User::factory()->create();
+        $user->assignRole(UserRole::USER);
+        $target = User::factory()->create();
+        $target->assignRole(UserRole::ADMIN);
+        $this->exhibition->users()->attach($target->id);
+
+        $this->actingAs($user)
+            ->delete("{$this->route}/{$target->id}")
+            ->assertForbidden();
+
+        $this->assertDatabaseHas('exhibition_user', [
+            'exhibition_id' => $this->exhibition->id,
+            'user_id'       => $target->id,
+        ]);
+    });
+
+    it('forbids guest users from entering the index page', function (): void {
+        get($this->route)
+            ->assertRedirect(route('login'));
+    });
+
+    it('forbids guest users from adding an admin', function (): void {
+        $user = User::factory()->create();
+        $user->assignRole(UserRole::USER);
+
+        \Pest\Laravel\put("{$this->route}/{$user->id}")
+            ->assertRedirect(route('login'));
+
+        $user->refresh();
+        $this->expect($user->role)->toBe(UserRole::USER);
+    });
+
+    it('forbids guest users from deleting an admin', function (): void {
+        $target = User::factory()->create();
+        $target->assignRole(UserRole::ADMIN);
+        $this->exhibition->users()->attach($target->id);
+
+        \Pest\Laravel\delete("{$this->route}/{$target->id}")
+            ->assertRedirect(route('login'));
+
+        $this->assertDatabaseHas('exhibition_user', [
+            'exhibition_id' => $this->exhibition->id,
+            'user_id'       => $target->id,
+        ]);
+    });
+
+    it('can assign one admin to multiple exhibitions', function (): void {
+        $user = User::factory()->create();
+        $user->assignRole(UserRole::USER);
+
+        $exhibition2 = Exhibition::factory()->create();
+
+        $this->actingAs($this->superAdmin)
+            ->put("{$this->route}/{$user->id}")
+            ->assertRedirect();
+
+        $this->actingAs($this->superAdmin)
+            ->put("/admin/exhibitions/{$exhibition2->id}/admins/{$user->id}")
+            ->assertRedirect();
+
+        $user->refresh();
+        $this->expect($user->exhibitions)->toHaveCount(2)
+            ->and($user->role)->toBe(UserRole::ADMIN);
+    });
+
+    it('does not downgrade the admin to the user after deleting them if they still have another exhibition assigned', function (): void {
+        $user = User::factory()->create();
+        $user->assignRole(UserRole::ADMIN);
+
+        $exhibition2 = Exhibition::factory()->create();
+        $this->exhibition->users()->attach($user->id);
+        $exhibition2->users()->attach($user->id);
+
+        $this->actingAs($this->superAdmin)
+            ->delete("{$this->route}/{$user->id}")
+            ->assertRedirect();
+
+        $user->refresh();
+        $this->expect($user->role)->toBe(UserRole::ADMIN);
+        $this->assertDatabaseHas('exhibition_user', [
+            'exhibition_id' => $exhibition2->id,
+            'user_id'       => $user->id,
+        ]);
+    });
+
+    it('downgrades the admin to the user after deleting them if they do not have another exhibition assigned', function (): void {
+        $user = User::factory()->create();
+        $user->assignRole(UserRole::ADMIN);
+        $this->exhibition->users()->attach($user->id);
+
+        $this->actingAs($this->superAdmin)
+            ->delete("{$this->route}/{$user->id}")
+            ->assertRedirect();
+
+        $user->refresh();
+        $this->expect($user->role)->toBe(UserRole::USER);
+        $this->assertDatabaseMissing('exhibition_user', [
+            'exhibition_id' => $this->exhibition->id,
+            'user_id'       => $user->id,
+        ]);
+    });
+
+    it('upgrades the user to the admin after assigning an exhibition to it', function (): void {
+        $user = User::factory()->create();
+        $user->assignRole(UserRole::USER);
+
+        $this->actingAs($this->superAdmin)
+            ->put("{$this->route}/{$user->id}")
+            ->assertRedirect();
+
+        $user->refresh();
+        $this->expect($user->role)->toBe(UserRole::ADMIN);
+        $this->assertDatabaseHas('exhibition_user', [
+            'exhibition_id' => $this->exhibition->id,
+            'user_id'       => $user->id,
+        ]);
+    });
 });
