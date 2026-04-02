a1e7ec82fafbbb8fc6db41de455087f9d224f0eb
Neoscripter <sange0337@gmail.com>
2026-04-02 15:58:28 +0800

    Style:make highlighted partner stand out on the user companies index page

--- resources/js/pages/user/Companies/Index.tsx
+++ resources/js/pages/user/Companies/Index.tsx
@@ -1,24 +1,21 @@
-import CardLayout from '@/components/layout/CardLayout';
 import AccentHeading from '@/components/ui/AccentHeading';
-import Image from '@/components/ui/Image';
 import SearchInput from '@/components/ui/SearchInput';
-import { cn } from '@/lib/utils';
-import { show } from '@/wayfinder/routes/companies';
 import { App } from '@/wayfinder/types';
-import { Link } from '@inertiajs/react';
 import { FC } from 'react';
+import PartnerCard from './partials/PartnerCard';
 
 const Index: FC<{
     exhibition: App.Models.Exhibition;
     companies: App.Models.Company[] | undefined;
 }> = ({ exhibition, companies }) => {
+
     const highlight = companies?.findIndex((company) =>
         company?.tags?.some(
             (tag) => tag.name.toLowerCase() === 'генеральный партнер',
         ),
     );
 
-    if (highlight && companies) {
+    if (highlight && highlight !== -1 && companies) {
         companies = [
             companies[highlight],
             ...companies.slice(0, highlight),
@@ -39,55 +36,20 @@ const Index: FC<{
                 <SearchInput placeholder="Поиск по партнерам" />
             </header>
 
-            <ul className="grid grid-cols-[repeat(auto-fill,minmax(15rem,1fr))] gap-8">
-                {companies?.map((company, idx) => (
-                    <li
-                        key={company.id}
-                        className={cn(
-                            idx === 0 && 'sm:col-span-2 sm:row-span-2',
-                        )}
-                    >
-                        <CardLayout className="relative w-full p-5 text-foreground ring-primary transition-transform hover:scale-103 hover:ring-2">
-                            {company.logo && (
-                                <Image
-                                    wrapperStyles={cn(
-                                        'mb-4 w-full',
-                                        idx === 0 && 'mb-8',
-                                    )}
-                                    imgStyles="object-contain"
-                                    image={company.logo}
-                                />
-                            )}
-
-                            <Link
-                                href={show({ exhibition, company }).url}
-                                className="absolute inset-0"
-                            />
-
-                            <h3
-                                className={cn(
-                                    'mx-auto mb-8 min-h-[3.2em] max-w-4/5 text-center font-bold',
-                                    idx === 0 && 'text-2xl',
-                                )}
-                            >
-                                {' '}
-                                {company.public_name}
-                            </h3>
-
-                            <p
-                                className={cn(
-                                    'min-h-[3.2em] text-center text-sm',
-                                    idx === 0 && 'text-lg',
-                                )}
-                            >
-                                {company.tags
-                                    ?.map((tag) => tag.name)
-                                    .join(', ')}
-                            </p>
-                        </CardLayout>
-                    </li>
-                ))}
-            </ul>
+            {companies && companies.length > 0 ? (
+                <ul className="grid gap-8 sm:grid-cols-[repeat(auto-fill,minmax(15rem,1fr))]">
+                    {companies?.map((company, idx) => (
+                        <PartnerCard
+                            key={company.id}
+                            exhibition={exhibition}
+                            company={company}
+                            highlighted={idx === 0}
+                        />
+                    ))}
+                </ul>
+            ) : (
+                <p>Не найдено ни одной компании</p>
+            )}
         </div>
     );
 };
---
+++ resources/js/pages/user/Companies/partials/PartnerCard.tsx
@@ -0,0 +1,66 @@
+import CardLayout from '@/components/layout/CardLayout';
+import Image from '@/components/ui/Image';
+import { cn } from '@/lib/utils';
+import { NodeProps } from '@/types/shared';
+import { show } from '@/wayfinder/routes/companies';
+import { App } from '@/wayfinder/types';
+import { Link } from '@inertiajs/react';
+import { FC } from 'react';
+
+const PartnerCard: FC<
+    NodeProps<{
+        company: App.Models.Company;
+        exhibition: App.Models.Exhibition;
+        highlighted?: boolean;
+    }>
+> = ({ className, exhibition, company, highlighted = false }) => {
+    return (
+        <li
+            className={cn(highlighted && 'sm:col-span-2 sm:row-span-2')}
+        >
+            <CardLayout
+                className={cn(
+                    'relative w-full p-5 text-foreground ring-primary transition-transform hover:scale-103 hover:ring-2',
+                    className,
+                )}
+            >
+                {company.logo && (
+                    <Image
+                        wrapperStyles={cn(
+                            'mb-4 w-full',
+                            highlighted ? 'w-full sm:mb-6' : 'max-w-3/5',
+                        )}
+                        imgStyles="object-contain"
+                        image={company.logo}
+                    />
+                )}
+
+                <Link
+                    href={show({ exhibition, company }).url}
+                    className="absolute inset-0"
+                />
+
+                <h3
+                    className={cn(
+                        'mx-auto mb-8 min-h-[3.2em] max-w-4/5 text-center font-bold',
+                        highlighted && 'sm:text-2xl',
+                    )}
+                >
+                    {' '}
+                    {company.public_name}
+                </h3>
+
+                <p
+                    className={cn(
+                        'min-h-[3.2em] text-center text-sm',
+                        highlighted && 'text-lg',
+                    )}
+                >
+                    {company.tags?.map((tag) => tag.name).join(', ')}
+                </p>
+            </CardLayout>
+        </li>
+    );
+};
+
+export default PartnerCard;
