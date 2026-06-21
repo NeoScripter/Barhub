94bb83597163ab1052462d5aaedaa0ba308d32c6
Neoscripter <sange0337@gmail.com>
2026-06-21 15:41:00 +0800

    Feat:create ui for integration page

--- app/Http/Controllers/Admin/IntergrationController.php
+++ app/Http/Controllers/Admin/IntergrationController.php
@@ -11,9 +11,11 @@ final class IntergrationController extends Controller
 {
     public function index()
     {
+        $path = storage_path() . '/logs/integration.log';
+        exec("tail -n 15 {$path}", $output);
 
         return Inertia::render('admin/Integration/Index', [
-            'logs' => 'logs',
+            'output' => $output,
         ]);
     }
 }
--- resources/js/pages/admin/Integration/Index.tsx
+++ resources/js/pages/admin/Integration/Index.tsx
@@ -1,7 +1,50 @@
-import { FC } from 'react';
+import AccentHeading from '@/components/ui/AccentHeading';
+import { Button } from '@/components/ui/Button';
+import RadioCheckbox from '@/components/ui/RadioCheckbox';
+import { FC, useState } from 'react';
 
-const Index: FC<{ logs: string }> = ({ logs }) => {
-    return 'hello world';
+const Index: FC<{ output: string[] }> = ({ output }) => {
+    const [on, setOn] = useState(false);
+
+    return (
+        <div className="space-y-8 md:space-y-12">
+            <div className="">
+                <AccentHeading
+                    asChild
+                    className="mb-1 text-lg"
+                >
+                    <h2>Мониторинг интеграции</h2>
+                </AccentHeading>
+            </div>
+
+            <div className="md:col-span-2">
+                <RadioCheckbox
+                    label="Статус публикации"
+                    value={on}
+                    onChange={(val) => setOn(val)}
+                />
+            </div>
+
+            <div className="flex flex-wrap items-center gap-4 md:gap-8">
+                <Button size="lg">Обновить</Button>
+                <Button
+                    size="lg"
+                    variant="secondary"
+                >
+                    Синхронизация
+                </Button>
+            </div>
+
+            <div>
+                <h3 className="mb-3 text-xl font-bold">Логи</h3>
+                <ul className="max-w-250 rounded-md border border-black p-4">
+                    {output.map((line, idx) => (
+                        <li key={idx}>{line}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
 };
 
 export default Index;
